481afec40773647adb75b15a5c060ad7d793ae23
alexsandron3 <alexsandro060299@outlook.com>
2022-07-07 22:38:58 -0300

    feat: cria classe TV com método de aumentar volume e testes

---
+++ classe_TV/src/TV.py
@@ -0,0 +1,10 @@
+class TV:
+    def __init__(self, tamanho):
+        self._volume = 50
+        self._canal = 1
+        self._tamanho = tamanho
+        self._ligada = False
+
+    def aumentar_volume(self):
+        if self._volume < 99:
+            self._volume += 1
---
+++ classe_TV/tests/test_TV.py
@@ -0,0 +1,21 @@
+from src.TV import TV
+
+
+def test_if_class_initialize_with_corrects_attrs():
+    tv = TV(10)
+    assert tv._volume == 50
+    assert tv._canal == 1
+    assert tv._tamanho == 10
+    assert tv._ligada == False
+
+
+def test_if_increase_volume_work_as_expected():
+    tv = TV(10)
+    tv.aumentar_volume()
+
+    assert tv._volume == 51
+
+    for _ in range(9):
+        tv.aumentar_volume()
+
+    assert tv._volume == 60
